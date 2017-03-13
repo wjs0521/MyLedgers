e9624fb2bab9dabfac7e44a41d7a80131f9025ab
wjs0521 <wjs0521@gmail.com>
2017-03-13 23:43:26 +0800

    2.Add SkillTreeHomeWork Local DB 2.Create EF:AccountBookModel.edmx 2.Create Folder:Models/Services 2.Create Service:MyLedgersService.cs 2.Edit Controller:MyLedgersController.cs

--- MyLedgers/Controllers/MyLedgersController.cs
+++ MyLedgers/Controllers/MyLedgersController.cs
@@ -5,33 +5,38 @@ using System.Web;
 using System.Web.Mvc;
 
 using MyLedgers.Models.ViewModels;
+using MyLedgers.Models.Services;
 
 namespace MyLedgers.Controllers
 {
     public class MyLedgersController : Controller
     {
+        MyLedgersService service = new MyLedgersService();
+
         // GET: MyLedgers
         public ActionResult Index()
         {
-            List<Ledgers> datalist = new List<Ledgers>();
-            Ledgers data1 = new Ledgers();
-            data1.Category = "支出";
-            data1.Amount = 300;
-            data1.PriceDate = DateTime.Parse("2016-01-01"); ;
-            data1.Remark = "Test1";
-            datalist.Add(data1);
-            Ledgers data2 = new Ledgers();
-            data2.Category = "支出";
-            data2.Amount = 16000;
-            data2.PriceDate = DateTime.Parse("2016-01-02");
-            data2.Remark = "Test2";
-            datalist.Add(data2);
-            Ledgers data3 = new Ledgers();
-            data3.Category = "支出";
-            data3.Amount = 8000;
-            data3.PriceDate = DateTime.Parse("2016-01-03");
-            data3.Remark = "Test3";
-            datalist.Add(data3);
+            //List<Ledgers> datalist = new List<Ledgers>();
+            //Ledgers data1 = new Ledgers();
+            //data1.Category = "支出";
+            //data1.Amount = 300;
+            //data1.PriceDate = DateTime.Parse("2016-01-01"); ;
+            //data1.Remark = "Test1";
+            //datalist.Add(data1);
+            //Ledgers data2 = new Ledgers();
+            //data2.Category = "支出";
+            //data2.Amount = 16000;
+            //data2.PriceDate = DateTime.Parse("2016-01-02");
+            //data2.Remark = "Test2";
+            //datalist.Add(data2);
+            //Ledgers data3 = new Ledgers();
+            //data3.Category = "支出";
+            //data3.Amount = 8000;
+            //data3.PriceDate = DateTime.Parse("2016-01-03");
+            //data3.Remark = "Test3";
+            //datalist.Add(data3);
+
+            var datalist = service.GetLists();
 
             return View(datalist);
         }
---
+++ MyLedgers/Models/Services/MyLedgersServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using MyLedgers.Models;
+using MyLedgers.Models.ViewModels;
+
+namespace MyLedgers.Models.Services
+{
+    public class MyLedgersService
+    {
+        private SkillTreeHomeworkEntities db = new SkillTreeHomeworkEntities();
+
+        public List<Ledgers> GetLists()
+        {
+            List<Ledgers> datalist = new List<Ledgers>();
+            foreach (var item in db.AccountBook)
+            {
+                Ledgers data = new Ledgers();
+                if (item.Categoryyy == 0)
+                {
+                    data.Category = "支出";
+                }
+                if (item.Categoryyy == 1)
+                {
+                    data.Category = "收入";
+                }
+                data.Amount = item.Amounttt;
+                data.PriceDate = item.Dateee;
+                data.Remark = item.Remarkkk;
+                datalist.Add(data);
+            }
+
+            return datalist;
+        }
+    }
+}
