2e06fddeafe6602809c39d1265a5d0f736663dac
wjs0521 <wjs0521@gmail.com>
2017-03-13 21:55:40 +0800

    1.Create ViewModels Folder => Class Ledgers.cs 1.Create Controller : MyLedgersController 1.Create Layout : _MyLedgersLayout.cshtml 1.Create Child Action : CreateLedger.cshtml 1.Create View : Index.cshtml

---
+++ MyLedgers/Controllers/MyLedgersController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MyLedgers.Models.ViewModels;
+
+namespace MyLedgers.Controllers
+{
+    public class MyLedgersController : Controller
+    {
+        // GET: MyLedgers
+        public ActionResult Index()
+        {
+            List<Ledgers> datalist = new List<Ledgers>();
+            Ledgers data1 = new Ledgers();
+            data1.Category = "支出";
+            data1.Amount = 300;
+            data1.PriceDate = DateTime.Parse("2016-01-01"); ;
+            data1.Remark = "Test1";
+            datalist.Add(data1);
+            Ledgers data2 = new Ledgers();
+            data2.Category = "支出";
+            data2.Amount = 16000;
+            data2.PriceDate = DateTime.Parse("2016-01-02");
+            data2.Remark = "Test2";
+            datalist.Add(data2);
+            Ledgers data3 = new Ledgers();
+            data3.Category = "支出";
+            data3.Amount = 8000;
+            data3.PriceDate = DateTime.Parse("2016-01-03");
+            data3.Remark = "Test3";
+            datalist.Add(data3);
+
+            return View(datalist);
+        }
+
+        public ActionResult CreateLedger()
+        {
+            List<SelectListItem> CategoryList = new List<SelectListItem>();
+
+            CategoryList.Add(new SelectListItem()
+            {
+                Text = "請選擇",
+                Value = "請選擇",
+                Selected = true
+            });
+
+            CategoryList.Add(new SelectListItem()
+            {
+                Text = "支出",
+                Value = "支出",
+                Selected = false
+            });
+
+            CategoryList.Add(new SelectListItem()
+            {
+                Text = "收入",
+                Value = "收入",
+                Selected = false
+            });
+
+            ViewBag.Category = CategoryList;
+
+            return View();
+        }
+    }
+}
---
+++ MyLedgers/Models/ViewsModels/Ledgers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyLedgers.Models.ViewModels
+{
+    public class Ledgers
+    {
+        [Display(Name = "類別")]
+        public string Category { get; set; }
+        [Display(Name = "金額")]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int Amount { get; set; }
+        [Display(Name = "日期")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime PriceDate { get; set; }
+        [Display(Name = "備註")]
+        public string Remark { get; set; }
+    }
+}
